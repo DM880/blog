9cf3cbb1a6fa38008db9db5b631580f66569479a
DM880 <denys.mauro@gmail.com>
2022-07-19 14:05:56 +0000

    Tue, Jul 19, 2022, 2:05 PM +00:00

--- src/data/story/models.py
+++ src/data/story/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+import datetime
+
 
 class Category(models.Model):
-    name = 
+    name =  models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.TextField(max_lenght=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    date = models.DateTimeField(blank=True, null=True, default=datetime.datetime.now)
+    views = models.IntegerField()
+
+
+class EntryPost(models.Model):
+    name = models.TextField(max_length=100)
+    content = models.TextField()
+
+
+class ImagePost(models.Model):
+    post = models.ForeignKey(EntryPost, on_delete=models.CASCADE, related_name="image")
+    image = models.ImageField(upload_to="post/")
