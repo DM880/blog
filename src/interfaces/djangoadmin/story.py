c068f598e7d38d20c0828918e852565818ac1175
DM880 <denys.mauro@gmail.com>
2022-07-19 14:20:00 +0000

    Tue, Jul 19, 2022, 2:20 PM +00:00

--- src/interfaces/djangoadmin/story.py
+++ src/interfaces/djangoadmin/story.py
@@ -4,3 +4,11 @@ from django.contrib import admin
 from src.data.story.models import Category,Post,Entry,Image,Comment
 
 
+class EntryInLine(admin.StackedInLine):
+    model = Entry
+
+    inlines = ImageInLine
+
+class ImageInLine(admin.)
+
+class PostAdmin(admin.ModelAdmin):
