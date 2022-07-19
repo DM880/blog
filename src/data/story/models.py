6e8da211e43e5a03eeb1ad277751fdedabbd14e3
DM880 <denys.mauro@gmail.com>
2022-07-19 14:16:00 +0000

    Tue, Jul 19, 2022, 2:16 PM +00:00

--- src/data/story/models.py
+++ src/data/story/models.py
@@ -13,14 +13,20 @@ class Post(models.Model):
     title = models.TextField(max_lenght=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
     date = models.DateTimeField(blank=True, null=True, default=datetime.datetime.now)
-    views = models.IntegerField()
+    view = models.IntegerField()
 
 
-class EntryPost(models.Model):
+class Entry(models.Model):
     name = models.TextField(max_length=100)
     content = models.TextField()
 
 
-class ImagePost(models.Model):
+class Image(models.Model):
     post = models.ForeignKey(EntryPost, on_delete=models.CASCADE, related_name="image")
     image = models.ImageField(upload_to="post/")
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
