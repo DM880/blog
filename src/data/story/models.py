953ec5a954a8938c95aeee77c90c1706a208af97
DM880 <denys.mauro@gmail.com>
2022-07-19 14:48:49 +0000

    Tue, Jul 19, 2022, 2:48 PM +00:00

--- src/data/story/models.py
+++ src/data/story/models.py
@@ -6,8 +6,8 @@ import datetime
 class Category(models.Model):
     name =  models.CharField(max_length=20)
 
-    def __str__(self):
-        return f"{self.name}"
+    # def __str__(self):
+    #     return f"{self.name}"
 
 
 class Post(models.Model):
