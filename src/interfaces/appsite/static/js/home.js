e828f453265d4ac430bb6eb3cf938db38a20da4c
DM880 <denys.mauro@gmail.com>
2022-08-28 10:01:53 +0000

    cleaning css

--- src/interfaces/appsite/static/js/home.js
+++ src/interfaces/appsite/static/js/home.js
@@ -19,11 +19,9 @@ function openPopup(){
 
 
 function imgOnHover(post_id){
-    document.getElementById('post-description-'+post_id).style.opacity = 1;
     document.getElementById('post-img-'+post_id).style.opacity = 0.5;
 }
 
 function imgOnOut(post_id){
-    document.getElementById('post-description-'+post_id).style.opacity = 0;
     document.getElementById('post-img-'+post_id).style.opacity = 1;
 }
--- src/interfaces/appsite/static/js/sections.js
+++ src/interfaces/appsite/static/js/sections.js
@@ -1,9 +1,7 @@
 function imgOnHover(post_id){
-    document.getElementById('post-description-'+post_id).style.opacity = 1;
     document.getElementById('post-img-'+post_id).style.opacity = 0.5;
 }
 
 function imgOnOut(post_id){
-    document.getElementById('post-description-'+post_id).style.opacity = 0;
     document.getElementById('post-img-'+post_id).style.opacity = 1;
 }
